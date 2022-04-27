5401929dd5499c7e3f754ea4a6d3fdbe16eb94a6
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-04-27 23:50:49 +1200

    adds app inventory ui

---
+++ app/src/main/java/com/example/brightplate/Controllers/InventoryActivity.kt
@@ -0,0 +1,30 @@
+package com.example.brightplate.controllers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.brightplate.R
+
+class InventoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inventory)
+
+        val viewButton: Button = findViewById(R.id.view_inventory_btn)
+        val editButton: Button = findViewById(R.id.edit_inventory_btn)
+
+        viewButton.setOnClickListener {
+            val viewInventory = Intent(this@InventoryActivity, InventoryViewActivity::class.java)
+            startActivity(viewInventory)
+        }
+
+        editButton.setOnClickListener {
+            val editInventory = Intent(this@InventoryActivity, InventoryEditActivity::class.java)
+            startActivity(editInventory)
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/brightplate/Controllers/InventoryEditActivity.kt
+++ app/src/main/java/com/example/brightplate/Controllers/InventoryEditActivity.kt
@@ -1,12 +1,12 @@
-package com.example.brightplate.main
+package com.example.brightplate.controllers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.brightplate.R
 
-class MainActivity : AppCompatActivity() {
+class InventoryEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_inventory_edit)
     }
 }
