8f24923c59a07320c80caab8812b5ea4502f6a00
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-04-29 17:56:30 +1200

    adds functionality to allow the user to add ingredients to db

--- app/src/main/java/com/example/brightplate/Controllers/InventoryEditActivity.kt
+++ app/src/main/java/com/example/brightplate/Controllers/InventoryEditActivity.kt
@@ -4,40 +4,57 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.example.brightplate.Models.Ingredient
 import com.example.brightplate.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class InventoryEditActivity : AppCompatActivity() {
-    lateinit var ingredientName : EditText
-    lateinit var ingredientUnitOption : Spinner
-    lateinit var ingredientQuantity : EditText
-    lateinit var ingredientUnitType : String
-    lateinit var addBtn : Button
-    lateinit var removeBtn : Button
+    lateinit var addBtn: Button
+    lateinit var removeBtn: Button
+    lateinit var ingredientNameInput: EditText
+    lateinit var ingredientUnitOption: Spinner
+    lateinit var ingredientQuantity: EditText
+
+    var ingredientAmount: Double = 0.0
+    lateinit var ingredientUnitType: String
+    lateinit var ingredientName: String
+
+    lateinit var dbRef: DatabaseReference
+    var dbPath: String = "Inventory"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory_edit)
 
-        ingredientName = findViewById(R.id.IngredientNameInput)
+        ingredientNameInput = findViewById(R.id.IngredientNameInput)
         ingredientUnitOption = findViewById(R.id.IngredientUnitTypeSpinner)
         ingredientQuantity = findViewById(R.id.IngredientQuantityInput)
         addBtn = findViewById(R.id.IngredientAddButton)
         removeBtn = findViewById(R.id.IngredientRemoveButton)
 
-        var unitTypeOptions = arrayOf("grams", "kilogram", "liters", "milliliters")
-        ingredientUnitOption.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, unitTypeOptions)
+        var unitTypeOptions = arrayOf("grams", "kilograms", "liters", "milliliters")
+        ingredientUnitOption.adapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, unitTypeOptions)
+
+        dbRef = FirebaseDatabase.getInstance().getReference(this.dbPath)
 
         ingredientUnitOption.onItemSelectedListener = object : AdapterView.OnItemSelectedListener,
             AdapterView.OnItemClickListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 ingredientUnitType = unitTypeOptions.get(p2)
-                Toast.makeText(applicationContext, "Unit ${ingredientUnitType} Selected", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Unit ${ingredientUnitType} Selected",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("Not yet implemented")
-                Toast.makeText(applicationContext, "No Unit Type Selected", Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, "No Unit Type Selected", Toast.LENGTH_LONG)
+                    .show()
             }
 
             override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
@@ -46,14 +63,53 @@ class InventoryEditActivity : AppCompatActivity() {
 
         }
 
-        addBtn.setOnClickListener(){
-            Toast.makeText(applicationContext, "${ingredientName.text}, ${ingredientUnitType}, ${ingredientQuantity.text} SELECTED", Toast.LENGTH_LONG).show()
+        addBtn.setOnClickListener() {
+            ingredientName = ingredientNameInput.text.toString()
+            ingredientAmount = ingredientQuantity.text.toString().toDouble()
+            saveIngredient(ingredientName, ingredientAmount, ingredientUnitType)
         }
 
-        removeBtn.setOnClickListener(){
-            Toast.makeText(applicationContext, "Remove Button", Toast.LENGTH_LONG).show()
+        removeBtn.setOnClickListener() {
+            Toast.makeText(applicationContext, "Remove Button Selected", Toast.LENGTH_LONG).show()
         }
 
     }
+
+    fun saveIngredient(ingName: String, ingAmount: Double, ingUnitType: String) {
+
+        if (ingName.isEmpty() || ingUnitType.isEmpty()) {
+            Toast.makeText(
+                applicationContext,
+                "Please Make Sure All Fields Have Been Filled",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        if (ingAmount.isNaN()) {
+            Toast.makeText(
+                applicationContext,
+                "Please Input A Number Type Only For Ingredient Amount",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+//        val ingId = dbRef.push().key!!
+
+        val ingredient = Ingredient(ingName, ingUnitType, ingAmount)
+        dbRef.child(ingName).setValue(ingredient).addOnCompleteListener {
+            Toast.makeText(
+                applicationContext,
+                "${ingredientName.toUpperCase()} - ${ingredientAmount} ${ingredientUnitType.toUpperCase()} ADDED",
+                Toast.LENGTH_LONG
+            ).show()
+        }.addOnFailureListener {
+        }
+    }
+
+    fun checkExistingIngredients(ingName : String) : Boolean {
+
+        return false
+    }
+
 
 }
---
+++ app/src/main/java/com/example/brightplate/Models/Ingredient.kt
@@ -0,0 +1,8 @@
+package com.example.brightplate.Models
+
+data class Ingredient(
+//    var ingId : String? = null,
+    var ingName : String? = null,
+    var ingUnit : String? = null,
+    var ingAmount : Double? = null
+)
