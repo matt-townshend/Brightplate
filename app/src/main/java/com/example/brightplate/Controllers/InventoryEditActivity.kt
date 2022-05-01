4c7d3a611a088e1bd6bc2fb1159050c09b6b6f9b
Ajit Singh <Ajit_Singh_1@outlook.com>
2022-05-01 22:26:31 +1200

    adding code documentation to the edit inventory

--- app/src/main/java/com/example/brightplate/Controllers/InventoryEditActivity.kt
+++ app/src/main/java/com/example/brightplate/Controllers/InventoryEditActivity.kt
@@ -1,5 +1,11 @@
 package com.example.brightplate.controllers
 
+/**
+ * @author Ajit Singh
+ * User Edit Inventory
+ * Performs add and remove ingredient operations
+ */
+
 import android.os.Bundle
 import android.view.View
 import android.widget.*
@@ -20,25 +26,26 @@ class InventoryEditActivity : AppCompatActivity() {
     private lateinit var ingredientQuantity: EditText
 
     private var ingredientAmount: Double = 0.0
-    private lateinit var ingredientUnitType: String
     private lateinit var ingredientName: String
+    private lateinit var ingredientUnitType: String
 
-    private lateinit var dbRef: DatabaseReference
     private lateinit var userId: String
-    private var dbPath: String = "Inventory"
     private lateinit var auth: FirebaseAuth
+    private var dbPath: String = "Inventory"
+    private lateinit var dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory_edit)
 
+        // init values
         ingredientNameInput = findViewById(R.id.IngredientNameInput)
         ingredientUnitOption = findViewById(R.id.IngredientUnitTypeSpinner)
         ingredientQuantity = findViewById(R.id.IngredientQuantityInput)
         addBtn = findViewById(R.id.IngredientAddButton)
         removeBtn = findViewById(R.id.IngredientRemoveButton)
 
-        var unitTypeOptions = arrayOf("grams", "kilograms", "liters", "milliliters")
+        var unitTypeOptions = arrayOf("grams", "kilograms", "liters", "milliliters", "each")
         ingredientUnitOption.adapter =
             ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, unitTypeOptions)
 
@@ -58,19 +65,23 @@ class InventoryEditActivity : AppCompatActivity() {
             override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {}
         }
 
-        addBtn.setOnClickListener() {
+        addBtn.setOnClickListener {
             ingredientName = ingredientNameInput.text.toString().toLowerCase()
             ingredientAmount = ingredientQuantity.text.toString().toDouble()
             saveIngredient(ingredientName, ingredientAmount, ingredientUnitType)
         }
 
-        removeBtn.setOnClickListener() {
+        removeBtn.setOnClickListener {
             ingredientName = ingredientNameInput.text.toString().toLowerCase()
             ingredientAmount = ingredientQuantity.text.toString().toDouble()
             removeIngredient(ingredientName, ingredientAmount, ingredientUnitType)
         }
     }
 
+    /**
+     *
+     * @return userID of the user logged in
+     */
     fun getUserId(): String {
         auth = FirebaseAuth.getInstance()
         var db = FirebaseDatabase.getInstance().getReference("users")
@@ -78,6 +89,12 @@ class InventoryEditActivity : AppCompatActivity() {
         return userId
     }
 
+    /**
+     *  @param ingName
+     *  @param ingAmount
+     *  @param ingUnitType
+     *  saves the ingredient values given by the user into the firebase database named "Inventory"
+     */
     fun saveIngredient(ingName: String, ingAmount: Double, ingUnitType: String) {
 
         if (ingName.isEmpty() || ingUnitType.isEmpty()) {
@@ -95,15 +112,17 @@ class InventoryEditActivity : AppCompatActivity() {
         }
 
         dbRef = FirebaseDatabase.getInstance().getReference(this.dbPath)
-
         dbRef.child(getUserId()).child(ingName).get().addOnSuccessListener {
             var dbIngName = it.child("ingName").value.toString()
 
+            // if the ingredient already exists in the database then the current ingredient's
+            // amount value is update appropriately
             if (dbIngName == ingName) {
                 var amount = it.child("ingAmount").value.toString().toDouble()
                 var unit = it.child("ingUnit").value.toString()
                 var newAmount: Double
 
+                // checks that the unit type for the ingredient matches
                 if (ingUnitType != unit) {
                     Toast.makeText(
                         applicationContext,
@@ -121,20 +140,27 @@ class InventoryEditActivity : AppCompatActivity() {
                     ).show()
                 }
             }
-
-        }.addOnFailureListener {
-            val ingredient = Ingredient(ingName, ingUnitType, ingAmount)
-            dbRef.child(getUserId()).child(ingName).setValue(ingredient)
-                .addOnCompleteListener {
-                    Toast.makeText(
-                        applicationContext,
-                        "${ingredientName.toUpperCase()} - ${ingredientAmount} ${ingredientUnitType.toUpperCase()} ADDED",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
         }
+
+        // if the ingredient does not exist, then it is added with the amount entered by the user
+        val ingredient = Ingredient(ingName, ingUnitType, ingAmount)
+        dbRef.child(getUserId()).child(ingName).setValue(ingredient)
+            .addOnCompleteListener {
+                Toast.makeText(
+                    applicationContext,
+                    "${ingredientName.toUpperCase()} - ${ingredientAmount} ${ingredientUnitType.toUpperCase()} ADDED",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
     }
 
+    /**
+     *  @param ingName
+     *  @param ingAmount
+     *  @param ingUnitType
+     *  removes the ingredient values given by the user into the firebase database named "Inventory"
+     */
     fun removeIngredient(ingName: String, ingAmount: Double, ingUnitType: String) {
         if (ingName.isEmpty() || ingUnitType.isEmpty()) {
             Toast.makeText(
@@ -149,6 +175,8 @@ class InventoryEditActivity : AppCompatActivity() {
                 var unit = it.child("ingUnit").value.toString()
                 var newAmount: Double
 
+                // if the amount is entered is between 0 and the ingredient amount on the database
+                // the ingredient database value is update appropriately
                 if (ingAmount < amount && ingAmount > 0.0) {
                     if (ingUnitType != unit) {
                         Toast.makeText(
@@ -157,6 +185,7 @@ class InventoryEditActivity : AppCompatActivity() {
                             Toast.LENGTH_SHORT
                         ).show()
                     } else {
+                        // subtracting the amount
                         newAmount = amount - ingAmount
                         dbRef.child(getUserId()).child(ingName).child("ingAmount")
                             .setValue(newAmount)
@@ -168,6 +197,7 @@ class InventoryEditActivity : AppCompatActivity() {
                         ).show()
                     }
 
+                    // if the amount is exactly equal to the total amount, then the whole value is removed
                 } else if (ingAmount == amount) {
                     if (ingUnitType != unit) {
                         Toast.makeText(
@@ -193,5 +223,4 @@ class InventoryEditActivity : AppCompatActivity() {
             }
         }
     }
-
 }
