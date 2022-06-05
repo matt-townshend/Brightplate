bb944249e7645e5bdfda0c6e1aaf5faee035e68a
matt-townshend <hgq6521@autuni.ac.nz>
2022-06-05 23:14:58 +1200

    added first attempt at code, unable to run emulator as of now so will troubleshoot then test

---
+++ app/src/main/java/com/example/brightplate/controllers/UpdateInventoryWhenCookedCallback.kt
@@ -0,0 +1,10 @@
+package com.example.brightplate.controllers
+
+
+// this interface is used to provide a synchronous approach to retrieving data from a firebase database snapshot
+// as firebase data snapshots are retrieved asynchronously, and code within android will continue
+// to execute while the data is being retrieved. a boolean flag will be returned from the object and
+// then stored here before being passed off to their respective activities/functions
+interface UpdateInventoryWhenCookedCallback {
+    fun onCallback(flag: Boolean)
+}
